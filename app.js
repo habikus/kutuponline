7c2272d55dea18ebd5b15a1da2c9c7e5c2f2cbe3
Muhammed Ali <habipyilmaz@hotmail.com>
2021-09-14 00:45:31 +0300

    3

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log("Örnek uygulama dinleniyor inşallah." + port);
+ // console.log("Örnek uygulama dinleniyor inşallah." + port);
 })
  
